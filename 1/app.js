85806c776d4637280a161869e7649f2c05eb3909
Daniyal-farooq <mdanifarooq@gmail.com>
2022-10-09 18:11:06 +0500

    Object Problem 1

---
+++ 1/app.js
@@ -0,0 +1,7 @@
+var student = {
+    name: "David Rayy",
+    sclass: "VI",
+    rollno: 12
+};
+var prop = Object.keys(student).join(" ");
+console.log(prop);
---
+++ 1/app.ts
@@ -0,0 +1,13 @@
+interface x {
+    name:string,
+    sclass:string,
+    rollno:number
+} 
+var student = {
+    name: "David Rayy",
+    sclass: "VI",
+    rollno: 12
+}
+let prop:string = Object.keys(student).join(" ");
+console.log(prop);
+
